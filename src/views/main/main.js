d0a28580cd0de402d0e1f589911ada8c32fd2ae2
Olga Volobueva <olvik1605@gmail.com>
2024-08-20 16:17:46 -0400

    add state, AppStateHook

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,31 @@
 import { AbstractView } from "../../common/view.js";
+import onChange from "on-change";
 
 export class MainView extends AbstractView {
-	constructor() {
+	state = {
+		list: [],
+		loading: false,
+		searchQuery: undefined,
+		offset: 0
+	}
+	constructor(appState) {
 		super();
-		this.setTitle('Book search')
+		this.appState = appState;
+		this.appState = onChange(this.appState, this.AppStateHook.bind(this));
+		this.setTitle('Book search');
+	}
+
+	AppStateHook(path) {
+		if (path === 'favorites') {
+			console.log(path);
+		}
 	}
 
 	render() {
 		const main = document.createElement('div');
-		main.innerHTML = 'Test';
+		main.innerHTML = `Number of books: ${this.appState.favorites.length}`;
 		this.app.innerHTML = '';
 		this.app.append(main);
+		this.appState.favorites.push('d');
 	}
 }
